9e00ab64ea1e8b1293cf970a2e04225a318cf4bf
Miste <misteboss1@gmail.com>
2018-06-29 21:12:50 +0200

    Uploading files

---
+++ src/Miste/autoupdater/AutoUpdater.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Miste\autoupdater;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class AutoUpdater extends PluginBase{
+
+	public $config;
+
+	public function onEnable() : void{
+
+		$this->getServer()->getPluginManager()->registerEvents(new EventHandler($this), $this);
+
+		if (!is_dir($this->getDataFolder())){
+            mkdir($this->getDataFolder());
+        }
+
+        $this->saveResource("config.yml");
+
+		$this->config = new Config($this->getDataFolder() . "config.yml");
+	}
+}
---
+++ src/Miste/autoupdater/EventHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Miste\autoupdater;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\UpdateNotifyEvent;
+use pocketmine\utils\Utils;
+
+class EventHandler implements Listener{
+
+	/** @var AutoUpdater */
+	private $plugin;
+
+    public function __construct(AutoUpdater $plugin){
+        $this->plugin = $plugin;
+    }
+
+    public function onUpdateNotifyEvent(UpdateNotifyEvent $event){
+    	$updateInfos = $event->getUpdater()->getUpdateInfo();
+
+        if($this->plugin->config->get("lastUpdateDate") !== $updateInfos["date"]){
+            $this->plugin->getLogger()->alert("A new update is available ! " . $updateInfos["base_version"] . " for " . $updateInfos["mcpe_version"]);
+            if($this->plugin->config->get("autoUpdate") === true){
+
+                $this->plugin->getLogger()->alert("Preparing the update..");
+                $this->plugin->getLogger()->alert("Disabling all the plugins..");
+
+                foreach($this->plugin->getServer()->getPluginManager()->getPlugins() as $plugin){
+                    if($plugin->getName() !== "AutoUpdater"){
+                        $this->plugin->getServer()->getPluginManager()->disablePlugin($plugin);
+                    }
+                }
+    
+
+                $phar = Utils::getURL($updateInfos["download_url"]);
+                file_put_contents($this->plugin->getServer()->getDataPath() . "PocketMine-MP.phar", $phar);
+
+                $this->plugin->config->set("lastUpdateDate", $updateInfos["date"]);
+                $this->plugin->config->save(false);
+    
+
+                $this->plugin->getServer()->getLogger()->alert("The server has been updated and is now restarting !");
+                $this->plugin->getServer()->shutdown();
+            }else{
+                $this->plugin->getLogger()->alert("If you want to update your software please change autoUpdate in config.yml to true.");
+            }
+        }
+    }
+}
